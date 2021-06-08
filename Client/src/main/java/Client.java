88e1cb198bc87d8be62abe0b4d48786a8e87c91a
Evgeny Nikolaev <nizhnii@mail.ru>
2021-06-08 22:56:40 +0300

    Добавлен проект Server и Client

---
+++ Client/src/main/java/Client.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static Socket clientSocket; // socket для общения
+    private static Scanner scanner;
+    private static BufferedReader in; // поток для чтения из
+    private static PrintWriter out; // поток для чтения в
+
+    public static void main(String[] args) {
+        int port = 8306;
+        String host = "netology.homework";
+        try {
+            try {
+                clientSocket = new Socket(host, port);
+                scanner = new Scanner(System.in);
+                // создаем потоки ввода/вывода
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
+                System.out.println("Напишем сообщение серверу: ");
+                String reader = scanner.nextLine();
+                //отправим сообщение серверу
+                out.write(reader + "\n");
+                out.flush();
+                //ответ сервера
+                System.out.println(in.readLine());
+                reader = scanner.nextLine();
+                out.write(reader + "\n");
+                out.flush();
+                System.out.println(in.readLine());
+                reader = scanner.nextLine();
+                out.write(reader + "\n");
+                out.flush();
+                System.out.println(in.readLine());
+            } finally {
+                System.out.println("Клиент был закрыт...");
+                clientSocket.close();
+                in.close();
+                out.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            ;
+        }
+    }
+}
---
+++ Server/src/main/java/Server.java
@@ -0,0 +1,61 @@
+/*в данном проекте реализованы две задания
+по теме Клиент-серверное приложение
+ */
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static ServerSocket server;  // socket сервера
+    private static Socket clientSocket;  // socket для общения
+    private static BufferedReader in; //поток для чтения из
+    private static PrintWriter out; //поток для записи в
+
+    public static void main(String[] args) {
+        int port = 8306;
+        try {
+            try {
+                server = new ServerSocket(port);
+                System.out.println("Сервер запущен");
+                System.out.println("Прослушиваем...");
+                clientSocket = server.accept();
+                // создаем потоки ввода/выввода
+                try {
+                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream())), true);
+                    //ожидаем сообщение от client
+                    String clintMessage = in.readLine();
+                    System.out.println("Сообщение клиента: " + clintMessage);
+                    //отправляем сообщение к client
+                    out.println(String.format("Hi %s, your port is %d. Как тебя зовут?", clintMessage, clientSocket.getPort()));
+                    out.flush();
+                    clintMessage = in.readLine();
+                    String nameClient = clintMessage;
+                    System.out.println("Сообщение клиента: " + clintMessage);
+                    out.println(String.format("Hi %s. Ты ребенок? (да/нет)", nameClient));
+                    out.flush();
+                    clintMessage = in.readLine();
+                    System.out.println("Сообщение клиента: " + clintMessage);
+                    if (clintMessage.equals("да")) {
+                        out.println(String.format("Добро пожаловать в детскую зону, %s! Давай играть", nameClient));
+                        out.flush();
+                    } else {
+                        out.println(String.format("Добро пожаловать во взрослую зону, %s! Хорошего отдыха", nameClient));
+                        out.flush();
+                    }
+                } finally {
+                    // закрываем socket
+                    clientSocket.close();
+                    //закрываем поток
+                    in.close();
+                    out.close();
+                }
+            } finally {
+                System.out.println("Сервер закрыт!");
+                server.close();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
